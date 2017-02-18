637e18540ce430400b10887ee81cbe9d4933a3ee
James Nylen <jnylen@gmail.com>
2017-02-18 19:13:17 +0000

    Initial commit

---
+++ bin/wp-mirror-status.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const github  = require( 'github' );
+const util    = require( 'util' );
+
+const config = require( '../config.json' );
+
+const gh = new github( {
+	version : '3.0.0'
+} );
+
+gh.authenticate( {
+	type  : 'oauth',
+	token : config.github.apiToken
+} );
+
+gh.repos.getCommits( {
+	owner    : 'WordPress',
+	repo     : 'wordpress-develop',
+	per_page : 1,
+}, ( err, commitsOfficial ) => {
+	if ( err ) {
+		throw err;
+	}
+
+	gh.repos.getCommits( {
+		owner    : 'nylen',
+		repo     : 'wordpress-develop-svn',
+		per_page : 1,
+	}, ( err, commitsMy ) => {
+		if ( err ) {
+			throw err;
+		}
+
+		const getRevision = commit => {
+			if ( ! commit ) {
+				throw new Error( 'Missing commit' );
+			}
+			const match = commit.commit.message.match(
+				/git-svn-id: https:\/\/develop\.svn\.wordpress\.org\/trunk@(\d+)/
+			);
+			if ( match ) {
+				return +match[ 1 ];
+			} else {
+				return 0;
+			}
+		};
+
+		const revisionOfficial = getRevision( commitsOfficial.data[ 0 ] );
+		const revisionMy       = getRevision( commitsMy.data[ 0 ] );
+
+		let descriptionMessage;
+		if ( revisionOfficial > revisionMy ) {
+			descriptionMessage = util.format(
+				'This repository is %d commit%s behind https://github.com/WordPress/wordpress-develop 😞',
+				revisionOfficial - revisionMy,
+				( revisionOfficial - revisionMy === 1 ? '' : 's' )
+			);
+		} else if ( revisionMy > revisionOfficial ) {
+			descriptionMessage = util.format(
+				'https://github.com/WordPress/wordpress-develop is %d commit%s behind this repository 😞',
+				revisionMy - revisionOfficial,
+				( revisionMy - revisionOfficial === 1 ? '' : 's' )
+			);
+		} else {
+			descriptionMessage = (
+				'This repository is up to date with https://github.com/WordPress/wordpress-develop ✨'
+			);
+		}
+
+		gh.repos.edit( {
+			owner       : 'nylen',
+			name        : 'wordpress-develop-svn',
+			repo        : 'wordpress-develop-svn',
+			description : descriptionMessage,
+		}, ( err, result ) => {
+			if ( err ) {
+				throw err;
+			}
+			// console.log( result );
+		} );
+	} );
+} );
